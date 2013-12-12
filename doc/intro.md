d69b65096fb086e082adf348d5ed2a30a4429056
obadz <dav-github@odav.org>
2013-12-12 11:51:45 +0000

    Commit the template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to xlloop-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/xlloop_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns xlloop-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [xlloop-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
